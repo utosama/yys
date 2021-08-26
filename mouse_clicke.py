7c044a431d95590fbdcb7e46f49aecdb7f99e97e
utosama <42053291+utosama@users.noreply.github.com>
2021-08-26 23:05:24 +0800

    Update mouse_clicke.py

--- mouse_clicke.py
+++ mouse_clicke.py
@@ -1,1 +1,29 @@
+import pyautogui, sys, time
+
+i = 0
+x = 1475
+y = 785
+time.sleep(2)
+try:
+    while i < 100:
+        # pyautogui.click(929,202)
+        # 点击开始
+        pyautogui.click(x, y)
+        print("第" + str(i + 1) + "次")
+        time.sleep(0.5)
+        pyautogui.click(x, y)
+        # 预计打完需要42s
+        time.sleep(45)
+        # 点击出御魂
+        pyautogui.click(x, y)
+        time.sleep(3)
+        pyautogui.click(x, y)
+        time.sleep(2)
+        pyautogui.click(x, y)
+        time.sleep(1)
+        pyautogui.click(x, y)
+        i += 1
+except Exception as e:
+    print(e)
+    sys.exit(0)
 
